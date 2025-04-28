a3a938712a562846c1478cae1b1e510acfe73ecf
Luke Gibson <luke.e.gibson@outlook.com>
2025-04-28 10:13:46 +1000

    Update app.tsx

--- src/HydraEditor/GUI/app.tsx
+++ src/HydraEditor/GUI/app.tsx
@@ -2,8 +2,6 @@ import Editor from "./pages/Editor";
 import { Link, Route, useRouter } from "./router";
 
 export default function App() {
-  const { path } = useRouter();
-
   return (
     <>
         <Editor />
